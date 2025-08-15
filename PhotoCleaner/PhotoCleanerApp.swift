c38d82d965ca17d7ef20294817f55b591928a6f2
action <alexemel2@yandex.ru>
2025-08-15 19:04:14 +0300

    Initial Commit

---
+++ PhotoCleaner/PhotoCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerApp.swift
+//  PhotoCleaner
+//
+//  Created by Алексей Емельянов on 15.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoCleanerTests/PhotoCleanerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerTests.swift
+//  PhotoCleanerTests
+//
+//  Created by Алексей Емельянов on 15.08.2025.
+//
+
+import Testing
+@testable import PhotoCleaner
+
+struct PhotoCleanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
